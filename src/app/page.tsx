02cfb2f4e2e669f406ed3aab22ebcec81e690e01
Jami <jamih@bu.edu>
2024-09-30 17:44:35 -0500

    added force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic"
+
 const mockUrls = [
   "https://utfs.io/f/9SbLlBPpHqRJVEpjwAXCK6gEwroZiOxvjU7C3nytmudh5qBR",
   "https://utfs.io/f/9SbLlBPpHqRJ46X3isFv3YVlbSg56qC4GoxrBcWXsePEdLMu",
